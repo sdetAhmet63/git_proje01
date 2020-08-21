249ba594a19faaab270f3dad5be576840cf7e14b
Ahmet <parisstar033@gmail.com>
2020-08-21 12:50:21 +0200

    Ikinci commit

--- src/git_proje01/FirstClass.java
+++ src/git_proje01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 	
 		System.out.println("Ïlk commit from local repository");
+		System.out.println("Ikinci commit from local repository");
 
 	}
 
